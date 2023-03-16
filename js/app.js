7cf1c1eacf65dbea2be14b94d68faf75950c0316
Mamun Rana <117065526+programmingmam@users.noreply.github.com>
2023-03-16 11:21:43 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+//  filtwe qullary
+
+    
+$(document).ready(function(){
+    $('.list').click(function(){
+        const value =$(this).attr('data-filter');
+        if (value == 'All'){
+
+            $('.itembox').show('1000');
+        }
+        else{
+            $('.itembox').not('.'+value).hide('1000');
+            $('.itembox').filter('.'+value).show('1000');
+        }
+    })
+    //add active class on selected item hmmm
+   
+}) 
+
+
+ 
