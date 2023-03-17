2756611e5e7f1962f2d40100b5f9b1e3de201c6e
Mamun Rana <117065526+programmingmam@users.noreply.github.com>
2023-03-17 09:29:00 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -19,3 +19,43 @@ $(document).ready(function(){
 
 
  
+// clint says slider
+
+const sliderContainer = document.querySelector('.slider-container');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const productCards = document.querySelectorAll('.product-card');
+
+let slideIndex = 0;
+const slideWidth = productCards[0].offsetWidth + 40;
+
+prevBtn.addEventListener('click', () => {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = productCards.length - 1;
+  }
+  sliderContainer.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+});
+
+nextBtn.addEventListener('click', () => {
+  slideIndex++;
+  if (slideIndex > productCards.length - 1) {
+    slideIndex = 0;
+  }
+  sliderContainer.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+});
+
+// end
+
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData.entries());
+  console.log(data);
+  // send form data to server using fetch or XMLHttpRequest
+}
+
+ 
